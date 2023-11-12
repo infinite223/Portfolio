9072d4cdd03310fb4b2c228c022bff5a64e3398b
infinite223 <dawidszmigiel9@gmail.com>
2023-11-12 16:10:00 +0100

    finished latestProject section

--- src/components/LatestProjectSection.vue
+++ src/components/LatestProjectSection.vue
@@ -1,38 +1,70 @@
 <script lang="ts" setup>
     import { useTheme } from '@/store';
     import scheduleApp from '../assets/scheduleApp.png'
-import { Icon } from '@iconify/vue/dist/iconify.js';
+    import { Icon } from '@iconify/vue/dist/iconify.js';
     const { theme } = useTheme()
 
 </script>
 
 <template>
     <div class="latestProjectSection">
-        <div class="headerText">Latest project</div>
+        <div style="color: gray;" class="headerText">Latest project</div>
         <div class="latestProjectSection__content">
             <img :src="scheduleApp" class="scheduleApp"/>
+            <div class="latestProjectSection__content-description">
+                <div class="latestProjectSection__content-nav">
+                    <Icon color="white" icon="bi:phone" class="icon" width="34px"/>
 
+                    <h2>
+                        ScheduleWork
+                    </h2>
+                </div>
+
+                <p>
+                    A mobile application for managing the work schedule of employees. It makes 
+                    it easier to record employees in the work schedule. The application allows 
+                    you to divide into groups, quickly view the next working days and other co-workers.
+                </p>
+            </div>
         </div>
         <div class="latestProjectSection__footer">
-            <div class="footer__item">
-                Mobile app
-                <Icon 
-                    icon="mdi:github" class="icon" width="24px"
-                />
-            </div>
-            <div class="footer__item">
-                Web app preview + download
-                <Icon 
-                    icon="mdi:link" class="icon" width="24px"
-                />
-            </div> 
-            
-            <div class="footer__item">
-                Backend
-                <Icon 
-                    icon="mdi:github" class="icon" width="24px"
-                />
-            </div>   
+            <a 
+                href="https://github.com/infinite223/Schedule-work-app"
+                target="_blank"
+                class="page-link" 
+            >
+                <div class="footer__item">
+                    Mobile app
+                    <Icon 
+                        icon="mdi:github" class="icon" width="24px"
+                    />
+                </div>
+            </a>
+            <a 
+                href="https://schedulework-preview.netlify.app/"
+                target="_blank"
+                class="page-link" 
+
+            >
+                <div class="footer__item">
+                    Web app preview + download
+                    <Icon 
+                        icon="mdi:link" class="icon" width="24px"
+                    />
+                </div> 
+            </a>
+            <a 
+                href="https://github.com/infinite223/Schedule-work"
+                target="_blank"
+                class="page-link" 
+            >
+                <div class="footer__item">
+                    Backend
+                    <Icon 
+                        icon="mdi:github" class="icon" width="24px"
+                    />
+                </div>   
+            </a>    
         </div>
     </div>
 </template>
@@ -43,17 +75,72 @@ import { Icon } from '@iconify/vue/dist/iconify.js';
         align-items: center;
         justify-content: center;
         flex-direction: column;
+        background-color:rgb(23, 23, 23);
+        
+        min-height: 100vh;
+        width: 95%;
+        margin: auto;
+        border-radius: 5px;
+        padding: 20px;
+        align-self: center;
 
-        height: 100vh;
-        width: 100%;
+        .page-link, .page-link:visited, .page-link:hover, .page-link:active, .page-link:focus {
+            outline: 0 none !important;
+            border: 0;
+            text-decoration: none;
+            border: none;
+        }
         .latestProjectSection__content {
             margin-top: 30px;
             display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 40px;
+
+            @media (max-width: 480px) {
+                width: 100%;
+                flex-direction: column;
+            }
             .scheduleApp {
                 width: 700px;
 
                 @media (max-width: 480px) {
-                    width: 95vw;
+                    width: 95%;
+                    flex-direction: column;
+                }
+            }
+
+            .latestProjectSection__content-description {
+                display: flex;
+                flex-direction: column;
+
+                gap: 10px;
+                padding: 20px;
+                height: fit-content;
+                width: 340px;
+                // background-color:rgb(23, 23, 23);
+                color: white;
+
+                p {
+                    font-size: 17px;
+                    font-weight: 100;
+                    color: rgb(187, 186, 186);
+                }
+
+                .latestProjectSection__content-nav {
+                    display: flex;
+                    align-items: center;
+                    gap: 15px;
+                    margin-left: -5px;
+
+                    h2 {
+                        color: white;
+                        font-size: 25px;
+                        font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+                        font-weight: 700;
+                        text-align: left;
+                        margin: 10px 0px;
+                    }
                 }
             }
         }
@@ -65,6 +152,7 @@ import { Icon } from '@iconify/vue/dist/iconify.js';
 
             @media (max-width: 480px) {
                 flex-direction: column;
+                width: 280px;
             }
 
             gap: 20px;
@@ -74,7 +162,8 @@ import { Icon } from '@iconify/vue/dist/iconify.js';
                 align-items: center;
                 justify-content: center;
                 gap: 20px;
-                background-color: rgba(144, 144, 144, 0.2);
+                // background-color: rgba(144, 144, 144, 0.2);
+                border: 1px solid rgb(184, 184, 184);
                 padding: 10px 25px;
                 color: rgb(214, 214, 214);
                 font-weight: 700;
